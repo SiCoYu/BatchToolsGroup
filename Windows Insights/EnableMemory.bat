aaef310808c3c6315d6ddb276b765c69824c7ba8
yuxikuo <yuxikuo@h3d.com.cn>
2024-06-20 11:32:35 +0800

    Editor PIE启动Insight Memory分析

--- Windows Insights/EnableMemory.bat
+++ Windows Insights/EnableMemory.bat
@@ -1,8 +1,9 @@
 taskkill /F /IM UnrealInsights.exe
 
-set InSightPath="E:\WorkTools\UE5\UE_5.1_T3\Engine\Binaries\Win64"
-set UE4EditorPath="D:\WP_YXK\UE_Src\UE_5.1_T3\Engine\Binaries\Win64\UnrealEditor.exe"
+set InSightPath="F:\UE5\UE_Src\UE_5.3_T3\Engine\Binaries\Win64"
+set UE4EditorPath="F:\UE5\UE_Src\UE_5.3_T3\Engine\Binaries\Win64\UnrealEditor.exe"
 
-start /d %InSightPath% UnrealInsights.exe -tracehost=127.0.0.1 -trace=default,memory
+start /d %InSightPath% UnrealInsights.exe -tracehost=127.0.0.1 -trace=default,memory &
 
+%UE4EditorPath%  D:/WP_YXK/UE5_WP/UT_UE5T3/UT_UE5T3.uproject -trace=default,memory
 pause
