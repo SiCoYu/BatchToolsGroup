9709beeb0bba126ce0fda662a3c5c3d7e585ca15
yuxikuo <yuxikuo@h3d.com.cn>
2024-07-08 13:46:16 +0800

    增加UE5 Android Insight方法

--- Android Insights/PushCommandLine.bat
+++ Android Insights/PushCommandLine.bat
@@ -40,17 +40,6 @@ echo %AppName%
 echo %PackName%
 echo %InSightPath%
 
-set TxtPath=%cd%\UECommandLine.txt
-
-rem start /d %InSightPath% UnrealInsights.exe
-
 adb.exe reverse tcp:1980 tcp:1980
 
 adb push UECommandLine.txt storage/emulated/0/Android/data/%PackName%/files/UnrealGame/%AppName%/
-
-rem adb shell setprop debug.unreal.commandline -tracehost=127.0.01
-
-rem adb shell am start -n %PackName%/com.epicgames.ue4.SplashActivity -tracehost=127.0.0.1 -trace=frame,cpu,gpu,memory
-adb shell am start -n %PackName%/com.epicgames.unreal.SplashActivity -tracehost=192.168.128.18 -trace=default,memory
-
-pause
